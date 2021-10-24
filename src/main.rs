e268539bcfbf6d7992915de64ac61107b8a87261
udon <myudongen@gmail.com>
2021-10-24 18:34:07 +0900

    add: init window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate bevy;
+
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::build()
+        .insert_resource(WindowDescriptor {
+            title: "Color Shooting".to_string(),
+            width: 960.,
+            height: 540.,
+            vsync: true,
+            ..Default::default()
+        })
+        .insert_resource(ClearColor(Color::rgb(1., 1., 1.)))
+        .add_plugins(DefaultPlugins)
+        .run();
 }
